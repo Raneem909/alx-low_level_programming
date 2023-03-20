23eb57db0a507a9a1f5f169947282cf3860bf323
Raneem909 <ranawad2002@gmail.com>
2023-03-21 00:34:27 +0200

    task1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  *Description: printing using puts finction
  *
- * Return : always 0
+ * Return: always 0
 */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
