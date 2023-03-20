5fcbe9c3467b4ba19100d248065b9bc2d08a2732
Raneem909 <ranawad2002@gmail.com>
2023-03-21 00:47:09 +0200

    task1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  *
  * Return: always(0)
 */
-int main()void
+int main(void)
 {
-        printf('with proper grammar, but the outcome is a piece of art,')
-        return (0)
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
