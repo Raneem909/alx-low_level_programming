a7400a6edf8721dbcd8a3288ce51396596c25b25
Raneem909 <ranawad2002@gmail.com>
2023-03-21 00:29:13 +0200

    task1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: printing using puts finction
+ *
+ * Return : always 0
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
