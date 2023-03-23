e8a25aaffd63a7ae66e7af3762eae6f7c33a36d9
Raneem909 <ranawad2002@gmail.com>
2023-03-23 02:59:51 +0200

    aa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: prints a-z
+ * Return: always 0
+*/
+int main(void)
+{
+int count;
+for (count = 'a'; count <= 'z'; count++)
+_putchar(count);
+_putchar('\n');
+return (0);
+}
