b0db31bd632b54762d44347433a798e9cb068312
Raneem909 <ranawad2002@gmail.com>
2023-03-25 04:06:19 +0200

    aa

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,9 +3,9 @@
  * print_most_numbers - prints nums 0-9 except2,4
  * Return: 0
 */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
-int num
+int num;
 for (num = 0; num <= 9; num++)
 if
 {
@@ -13,5 +13,5 @@ if
 continue;
 _putchar(num +48);
 }
-_putchar('\n')
+_putchar('\n');
 }
