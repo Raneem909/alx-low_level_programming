dc93061926e48230010a9f9bbd26e81dba39b9a5
Raneem909 <ranawad2002@gmail.com>
2023-03-28 14:31:46 +0200

    1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,7 +8,8 @@ void print_rev(char *s)
 {
 int count = 0;
 for (; *(s + count) != '\0'; count++);
-for (int i = count - 1; i >= 0; i--)
+int i;
+for (i = count - 1; i >= 0; i--)
 _putchar(s[i] + 0);
 _putchar('\n');
 }
