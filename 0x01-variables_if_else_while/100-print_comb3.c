20ccdb4451e643bf5a220c740ccb89ae61662790
Raneem909 <ranawad2002@gmail.com>
2023-04-02 22:46:37 +0200

    1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints 2 digits 
+ * combination 0f 0-9 seperated by ,
+ * Return: 0
+*/
+int main(void)
+{
+int digit = 0;
+for (; digit <= 9; digit++)
+{
+int digit2 = 0;
+for (; digit2 <= 9; digit2++)
+{
+if (digit != digit2 && digit < digit2)
+{
+putchar(digit + 48);
+putchar(digit2 + 48);
+if (digit + digit2 != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
