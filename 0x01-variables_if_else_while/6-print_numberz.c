e0552d120a824c5b6588047c8ed1764d6239fcb8
Raneem909 <ranawad2002@gmail.com>
2023-04-02 17:24:37 +0200

    1

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,9 +6,9 @@
 */
 int main(void)
 {
-int digit = 0;
-for (; digit < 10; digit++)
-printf("%d", digit);
+int digit = 48;
+for (; digit < 58; digit++)
+putchar(digit);
 putchar('\n');
 return (0);
 }
