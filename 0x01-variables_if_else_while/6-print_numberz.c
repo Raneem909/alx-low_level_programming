802d2208cede9d5de987d2bbe06e2dc63b2a0cc2
Raneem909 <ranawad2002@gmail.com>
2023-04-02 17:21:48 +0200

    1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints 0-9
+ * Return: 0
+*/
+int main(void)
+{
+int digit = 0;
+for (; digit < 10; digit++)
+printf("%d", digit);
+putchar('\n');
+return (0);
+}
