6a4675b29c65084c8a3c9b917bcae4b4ce03a299
Raneem909 <ranawad2002@gmail.com>
2023-04-02 17:20:10 +0200

    1

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,10 +4,10 @@
  * Description: prints 0-9
  * Return: 0
 */
-int main (void)
+int main(void)
 {
 int digit = 0;
-for (;digit > 10; digit++)
+for (; digit < 10; digit++)
 printf("%d", digit);
 putchar('\n');
 return (0);
