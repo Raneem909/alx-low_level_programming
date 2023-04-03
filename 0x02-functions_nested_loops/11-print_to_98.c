7594198237d1f7924e317de891e095baa6028bc1
Raneem909 <ranawad2002@gmail.com>
2023-04-04 01:15:16 +0200

    aa

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_to_98 - print numbers to 98
+ * @n: input
+ * Return: numbers to 98
+*/
+void print_to_98(int n)
+{
+for (n = 0; n = 98; n++)
+_putchar(n)
+return
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal line
+ * @n: input
+ * Return: result
+*/
+void print_diagonal(int n)
+{
+int ur_n, space;
+if (n <= 0)
+_putchar('\n')
+else
+{
+for ()
+}
+}
