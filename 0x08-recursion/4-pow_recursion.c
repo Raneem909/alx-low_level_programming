4a88bde75efe303c1f7f9c9b2f55d59f12108482
Raneem909 <ranawad2002@gmail.com>
2023-04-07 00:29:34 +0200

    1

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,7 +7,7 @@
  * @y: power number
  *
  * Return: if "y" > raised to the power of "y",
- * return -1 if "y" < 0, return 1 if "y" = 0 
+ * return -1 if "y" < 0, return 1 if "y" = 0
  */
 int _pow_recursion(int x, int y)
 {
