2adc52244ec767be9bd08494806c0c84297e385b
Raneem909 <ranawad2002@gmail.com>
2023-04-10 20:56:37 +0200

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * main - entry point
+ * @argv: array of entered arguments
+ * @argc: count for entered arguments
+ * Return: none
+*/
+int main(int argc, char const **argv)
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
