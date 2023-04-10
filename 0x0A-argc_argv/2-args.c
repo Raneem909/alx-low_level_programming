9d444f65351459097db9c0382d77729bef6c201d
Raneem909 <ranawad2002@gmail.com>
2023-04-10 21:17:02 +0200

    .

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+ * main - entry point
+ * @argv: array of entered arguments
+ * @argc: count for entered arguments
+ * Return: none
+*/
+int main(int argc, char const **argv)
+{
+int i;
+for (i = 1; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
