5ffa35b2d286b6e1158f6be114b0731b694a786b
Raneem909 <ranawad2002@gmail.com>
2023-04-12 01:09:28 +0200

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,13 +11,14 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+size_t len1, len2;
 char *new_str;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-size_t len1 = strlen(s1);
-size_t len2 = strlen(s2);
+len1 = strlen(s1);
+len2 = strlen(s2);
 new_str = (char *) malloc((len1 + len2 + 1));
 if (new_str == NULL)
 return (NULL);
