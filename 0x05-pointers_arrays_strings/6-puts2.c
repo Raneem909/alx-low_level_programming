a05d517f808a55bd1946e0fdf1fe49d07ca77f1d
Raneem909 <ranawad2002@gmail.com>
2023-04-16 00:55:06 +0200

    .

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,10 @@
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0' && str[i + 1] != '\0'; i += 2)
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
 _putchar(str[i] + 0);
+}
 _putchar('\n');
 }
