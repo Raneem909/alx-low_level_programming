5698a5356db53d78dad349e913205f4aac79a0a0
Raneem909 <ranawad2002@gmail.com>
2023-04-18 02:21:35 +0200

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
 #ifndef DOH_H
 #define DOG_H
-/*
+/**
  * struct dog - dog struct
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  * Description: dog info
- */
+*/
 struct dog
 {
 char *name;
 float age;
-char* owner;
+char *owner;
 };
 typedef struct dog dog_t;
 void print_dog(struct dog *d);
