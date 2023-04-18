453813210ee4cc88b51488c1a71f2518ff019535
Raneem909 <ranawad2002@gmail.com>
2023-04-18 02:15:00 +0200

    .

---
+++ dog.h
@@ -0,0 +1,19 @@
+#ifndef DOH_H
+#define DOG_H
+/*
+ * struct dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: dog info
+ */
+struct dog
+{
+char *name;
+float age;
+char* owner;
+};
+typedef struct dog dog_t;
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
