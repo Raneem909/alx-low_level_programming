2c11a49dc52def7907338e8e111b0850597e9a49
Raneem909 <ranawad2002@gmail.com>
2023-04-20 04:46:31 +0200

    .

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,6 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-unsigned int i;
-for (i = 0; i < size; i++)
-action(*array[i]);
+for (; array < size; array++)
+action(array);
 }
