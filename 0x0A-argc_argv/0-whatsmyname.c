45197901e7fc9b19a5db986bc1a2db1873f42ebe
Raneem909 <ranawad2002@gmail.com>
2023-07-09 20:11:42 +0200

    task 0 and 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: not used parameter but used for parameters counting
+ * @argv: array of poointers to the input strings
+ * Return: zero in sucess
+*/
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point for a function prints
+ * the passed arguments number
+ * @argc: argument count
+ * @argv: array of pointers to passed strings
+ * Return: zero in sucess
+*/
+int main (int argc, char **argv)
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
